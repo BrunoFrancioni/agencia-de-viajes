d56d0a1168e5af0d561bec10f60aa06eb35c89e9
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-04-21 09:40:19 -0300

    Pagina principal finalizada y partials de terminomiales y viajes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,25 @@ const Testimonial = require('../models/Testimoniales');
 
 module.exports = () => {
     router.get('/', (req, res) => {
-        res.render('index');
+        const promises = [];
+
+        promises.push(Viaje.findAll({
+            limit: 3
+        }))
+
+        promises.push(Testimonial.findAll({
+            limit: 3
+        }))
+
+        const resultado = Promise.all(promises);
+        
+        resultado
+            .then(resultado => res.render('index', {
+                clase: 'home',
+                viajes: resultado[0],
+                testimoniales: resultado[1]
+            }))
+            .catch(error => console.log(error))
     });
 
     router.get('/nosotros', (req, res) => {
